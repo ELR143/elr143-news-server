7233700394e8cf3a6a5a04b1d4aff830edd9032e
ELR143 <emmlou143@gmail.com>
2023-11-21 15:47:18 +0000

    fixes jest-sorted

--- __tests__/news-server.test.js
+++ __tests__/news-server.test.js
@@ -2,7 +2,7 @@ const app = require("../app");
 const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
-const sort = require("jest-sorted");
+
 const {
   articleData,
   commentData,
@@ -117,15 +117,15 @@ describe("GET /api/articles/:article_id/comments", () => {
             article_id: expect.any(Number),
           });
         });
-        expect(body.comments).toBeSortedBy('created_at', {descending: true})
+        expect(body.comments).toBeSortedBy("created_at", { descending: true });
       });
   });
   test("404: responds with an error message if passed an id that does not exist", () => {
     return request(app)
-    .get('/api/articles/99999/comments')
-    .expect(404)
-    .then(({body}) => {
-      expect(body.msg).toBe('Error: 404 not found')
-    })
+      .get("/api/articles/99999/comments")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Error: 404 not found");
+      });
   });
 });
